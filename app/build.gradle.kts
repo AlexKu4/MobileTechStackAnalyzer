1f4a4e16c4459abbd341da33aee8ddd7fdaa1f47
AlexKu4 <kuznetsov.kstovo52@gmail.com>
2026-04-18 15:12:42 +0300

    change the path of decoding keystore file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,7 +24,7 @@ android {
         minSdk = 24
         targetSdk = 34
         versionCode = 1
-        versionName = "v1.0.5"
+        versionName = "v1.0.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
