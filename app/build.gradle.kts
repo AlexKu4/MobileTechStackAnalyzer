3028f41e2124d46854da25c4fe0036a155e5b6fe
AlexKu4 <kuznetsov.kstovo52@gmail.com>
2026-05-08 23:47:56 +0300

    change icon

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,7 +23,7 @@ android {
         applicationId = "com.example.mobiletechstack"
         minSdk = 24
         targetSdk = 34
-        versionCode = 1
+        versionCode = 2
         versionName = "v1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
